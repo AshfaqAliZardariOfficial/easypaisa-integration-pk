3debd11702365f58a271bda6fa1e3fa7d7b097bf
Ashfaq Ali Zardari <ashfaqalizardariofficial@gmail.com>
2022-01-17 17:35:55 +0500

    This project contains Asp.Net MVC C# project with Easypaisa payment integration Pakistan.

---
+++ WPay/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WPay.Utils;
+
+namespace WPay.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult generate_hash(string textToEncrypt, string merchant_hash)
+        {
+            textToEncrypt = new AES_128_ECB_Manager().Encrypt(textToEncrypt, merchant_hash);
+            return Json(new { Success = true, Data = new { Code = 0, Message = textToEncrypt } }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ WPay/Utils/AES_128_ECB_Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WPay.Utils
+{
+    public class AES_128_ECB_Manager
+    {
+        public string Encrypt(string text, string text_key)
+        {
+            byte[] src = Encoding.UTF8.GetBytes(text);
+            byte[] key = Encoding.ASCII.GetBytes(text_key);
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.KeySize = 128;
+            using (ICryptoTransform encrypt = aes.CreateEncryptor(key, null))
+            {
+                byte[] dest = encrypt.TransformFinalBlock(src, 0, src.Length);
+                encrypt.Dispose();
+                return Convert.ToBase64String(dest);
+            }
+        }
+
+        public string Decrypt(string text, string text_key)
+        {
+            byte[] src = Convert.FromBase64String(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            byte[] key = Encoding.ASCII.GetBytes(text_key);
+            aes.KeySize = 128;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Mode = CipherMode.ECB;
+            using (ICryptoTransform decrypt = aes.CreateDecryptor(key, null))
+            {
+                byte[] dest = decrypt.TransformFinalBlock(src, 0, src.Length);
+                decrypt.Dispose();
+                return Encoding.UTF8.GetString(dest);
+            }
+        }
+    }
+}
